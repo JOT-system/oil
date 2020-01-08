fbde32755d2c79c738daf035d83392007866a03c
tmorikawa <tmorikawa@PC1567>
2020-01-08 09:59:42 +0900

    20200108_COMMIT

--- JOTWEB/OIL/mas/OIM0004StationList.aspx.designer.vb
+++ JOTWEB/OIL/mas/OIM0004StationList.aspx.designer.vb
@@ -50,6 +50,15 @@ Partial Public Class OIM0004StationList
     Protected WithEvents WF_SELUORG_TEXT As Global.System.Web.UI.WebControls.Label
     
     '''<summary>
+    '''WF_ListCNT コントロール。
+    '''</summary>
+    '''<remarks>
+    '''自動生成されたフィールド。
+    '''変更するには、フィールドの宣言をデザイナー ファイルから分離コード ファイルに移動します。
+    '''</remarks>
+    Protected WithEvents WF_ListCNT As Global.System.Web.UI.WebControls.Label
+    
+    '''<summary>
     '''WF_ButtonFIRST2 コントロール。
     '''</summary>
     '''<remarks>

eade89e042677cff67243ac70aebd4d9900f6950
hmiyake <hmiyake@PC1551>
2020-05-08 09:41:10 +0900

    ガイダンスマスタメンテ必要オブジェクト追加（ガワのみ）

---
+++ JOTWEB/OIL/mas/OIM0020GuidanceDownload.aspx.vb
@@ -0,0 +1,15 @@
+﻿''' <summary>
+''' ガイダンスダウンロードクラス(画面は提供せずファイルストリームを転送する)
+''' </summary>
+Public Class OIM0020GuidanceDownload
+    Inherits System.Web.UI.Page
+    ''' <summary>
+    ''' 
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+
+    End Sub
+
+End Class

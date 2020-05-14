676d6cbef2ff1484f0e657b101c343dce14877a6
tmorikawa <tmorikawa@PC1567>
2020-05-14 11:35:48 +0900

    20200513_指摘票対応(NO51)全体

--- JOTWEB/OIL/ord/OIT0003OrderDetail.aspx.vb
+++ JOTWEB/OIL/ord/OIT0003OrderDetail.aspx.vb
@@ -10002,6 +10002,17 @@ Public Class OIT0003OrderDetail
                             '出線列車名
                             updHeader.Item("LOADINGOUTLETTRAINNAME") = WW_GetValue(7)
 
+                            '★表の1行目を入力した場合、2行目以降の値も同様に設定する。
+                            If WW_LINECNT = 1 Then
+                                For Each OIT0003row As DataRow In OIT0003tbl_tab2.Rows
+                                    OIT0003row("LOADINGIRILINETRAINNO") = WW_SelectValue
+                                    OIT0003row("LOADINGIRILINETRAINNAME") = WW_SelectText
+                                    OIT0003row("LINE") = WW_GetValue(5)
+                                    OIT0003row("LOADINGOUTLETTRAINNO") = WW_GetValue(6)
+                                    OIT0003row("LOADINGOUTLETTRAINNAME") = WW_GetValue(7)
+                                Next
+                            End If
+
                             '積込出線列車番号を一覧に設定
                         ElseIf WF_FIELD.Value = "LOADINGOUTLETTRAINNO" Then
                             '〇 KeyCodeが重複し、名称(Value1)が異なる場合の取得術

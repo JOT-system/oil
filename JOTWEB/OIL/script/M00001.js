5660b2259fa8d40ec145b838065b341b3351958f
hmiyake <hmiyake@PC1551>
2020-05-20 09:39:27 +0900

    メニューのガイダンス表示非表示状態当日のみ保持

--- JOTWEB/OIL/script/M00001.js
+++ JOTWEB/OIL/script/M00001.js
@@ -9,18 +9,26 @@ function InitDisplay() {
 function bindShowCloseGuidance(objButton) {
     let menuBox = document.getElementById('Menuheaderbox');
     let guidanceAreaObj = document.getElementById('guidanceArea');
-    menuBox.classList.add('showGuidance');
-    objButton.textContent = '× ガイダンス非表示';
+    let flag = getDispGuigance();
+    if (flag === '1') {
+        menuBox.classList.add('showGuidance');
+        objButton.textContent = '× ガイダンス非表示';
+    } else {
+        objButton.textContent = '＋ ガイダンス表示';
+        guidanceAreaObj.style.height = '0px';
+    }
     objButton.addEventListener('click', (function (objButton, menuBox, guidanceAreaObj) {
         return function () {
             if (menuBox.classList.contains('showGuidance')) {
                 menuBox.classList.remove('showGuidance');
                 objButton.textContent = '＋ ガイダンス表示';
                 guidanceAreaObj.style.height = '0px';
+                setDispGuidance('0');
             } else {
                 menuBox.classList.add('showGuidance');
                 objButton.textContent = '× ガイダンス非表示';
                 guidanceAreaObj.style.height = '';
+                setDispGuidance('1');
             }
         };
     })(objButton, menuBox, guidanceAreaObj), true);
@@ -28,28 +36,28 @@ function bindShowCloseGuidance(objButton) {
 
 }
 
-//function getDispGuigance() {
-//    let dtm = localStorage.getItem("menu0001GuidanceSetDate");
-//    let flg = localStorage.getItem("menu0001GuidanceFlag");
-//    var dt = new Date();
-//    var y = dt.getFullYear();
-//    var m = ("00" + (dt.getMonth() + 1)).slice(-2);
-//    var d = ("00" + dt.getDate()).slice(-2);
-//    let currentDtm = y + m + d;
-//    if (dtm === null) {
-//        dtm = currentDtm;
-//        localStorage.setItem('menu0001GuidanceSetDate', dtm);
-//    }
-//    if (dtm === currentDtm) {
-//        if (flg !== null) {
-//            return flg;
-//        } else {
-//            return '1';
-//        }
-//    } else {
-//        localStorage.setItem('menu0001GuidanceSetDate', dtm);
-//    }
-//}
-//function setDispGuidance(flag) {
-
-//}
+function getDispGuigance() {
+    let dtm = localStorage.getItem("menu0001GuidanceSetDate");
+    let flg = localStorage.getItem("menu0001GuidanceFlag");
+    var dt = new Date();
+    var y = dt.getFullYear();
+    var m = ("00" + (dt.getMonth() + 1)).slice(-2);
+    var d = ("00" + dt.getDate()).slice(-2);
+    let currentDtm = y + m + d;
+    if (dtm === null) {
+        dtm = currentDtm;
+        localStorage.setItem('menu0001GuidanceSetDate', dtm);
+    }
+    if (dtm === currentDtm) {
+        if (flg === null) {
+            flg = '1';
+        }
+    } else {
+        localStorage.setItem('menu0001GuidanceSetDate', dtm);
+        localStorage.setItem("menu0001GuidanceFlag", flg);
+    }
+    return flg;
+}
+function setDispGuidance(flag) {
+    localStorage.setItem("menu0001GuidanceFlag", flag);
+}

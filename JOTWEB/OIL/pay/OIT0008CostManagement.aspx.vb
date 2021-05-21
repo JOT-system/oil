27a51bac466c5361063b1304a538f68fef3ef4f0
sigusa <sigusa@PC1560>
2021-05-21 11:12:32 +0900

    Revert "〇20210520_費用管理_計上年月テーブル項目変更に伴う修正"
    This reverts commit 01be6ee58db05475b2380087af5d6f61620a01f9.

--- JOTWEB/OIL/pay/OIT0008CostManagement.aspx.vb
+++ JOTWEB/OIL/pay/OIT0008CostManagement.aspx.vb
@@ -1143,8 +1143,6 @@ Public Class OIT0008CostManagement
         SQLStrBldr.AppendLine("     FORMAT(KEIJYOYM, 'yyyy/MM') AS KEIJYOYM")
         SQLStrBldr.AppendLine(" FROM")
         SQLStrBldr.AppendLine("     [oil].OIT0019_KEIJYOYM")
-        SQLStrBldr.AppendLine(" WHERE")
-        SQLStrBldr.AppendLine("     CAMPCODE = @P01")
 
         Try
             Using SQLcon As SqlConnection = CS0050SESSION.getConnection
@@ -1154,9 +1152,6 @@ Public Class OIT0008CostManagement
                 Using SQLcmd As New SqlCommand(SQLStrBldr.ToString(), SQLcon)
                     'SQL実行
                     Dim WK_TBL As DataTable = New DataTable()
-                    Dim PARA01 As SqlParameter = SQLcmd.Parameters.Add("@P01", SqlDbType.NVarChar, 2)
-                    PARA01.Value = Master.USERCAMP
-
                     Using SQLdr As SqlDataReader = SQLcmd.ExecuteReader()
                         '○ フィールド名とフィールドの型を取得
                         For index As Integer = 0 To SQLdr.FieldCount - 1
